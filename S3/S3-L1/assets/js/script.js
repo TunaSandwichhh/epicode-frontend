260f87e9b28acb065c9997e587bd0df3fdd496bc
TunaSandwich <davide.faggionato31@gmail.com>
2024-02-05 17:25:13 +0100

    //

--- S3/S3-L1/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-// let test = document.getElementById("test");
-// console.log(typeof test);
-
-let text = document.getElementById("text");
-console.log(text.id);
-
-text.id = "aaa";
-console.log(text.id);
