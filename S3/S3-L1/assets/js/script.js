9c1f497678583ae243627fa241c971004f58cff3
TunaSandwich <davide.faggionato31@gmail.com>
2024-02-05 15:04:14 +0100

    feat: implemented S3-L1

---
+++ S3/S3-L1/assets/js/script.js
@@ -0,0 +1,8 @@
+// let test = document.getElementById("test");
+// console.log(typeof test);
+
+let text = document.getElementById("text");
+console.log(text.id);
+
+text.id = "aaa";
+console.log(text.id);
