e697c9ee7bfc26c48b54f8233f93450d7574393c
TunaSandwich <davide.faggionato31@gmail.com>
2024-02-08 12:20:53 +0100

    Feat: completed S3-L4 live coding

--- S3/S3-L4-live-coding/assets/js/script.js
+++ S3/S3-L4-live-coding/assets/js/script.js
@@ -26,6 +26,7 @@ const printCurrentMonth = () => {
   const currentMonth = monthNames[getMonth];
   title.innerText = currentMonth;
 };
+printCurrentMonth();
 
 const dayInThisMonth = () => {
   const lastDayInMonth = new Date(getYear, getMonth + 1, 0); //ottengo l'ultimo giorno del mese corrente
@@ -43,7 +44,20 @@ const unselectAllDays = () => {
 const changeMeetingDaySection = (indexOfDay) => {
   const rightSpan = document.getElementById("new-meeting-day");
   rightSpan.classList.add("hasDay");
-  rightSpan.innerText = indexOfDay + 1;
+  rightSpan.innerText = `${indexOfDay + 1} ${monthNames[getMonth]}`;
+};
+
+const showAppointments = (indexOfDay) => {
+  const appointmentsForThisDay = appointments[indexOfDay];
+  const appointmentsList = document.querySelector("#appointments ul");
+  appointmentsList.innerHTML = "";
+  appointmentsForThisDay.forEach((appointment) => {
+    const newLi = document.createElement("li");
+    newLi.innerText = appointment;
+    appointmentsList.appendChild(newLi);
+  });
+  const appointmentDiv = document.getElementById("appointments");
+  appointmentDiv.style.display = "block";
 };
 
 const createDays = (numberOfDays) => {
@@ -55,6 +69,42 @@ const createDays = (numberOfDays) => {
       unselectAllDays();
       dayDiv.classList.add("selected");
       changeMeetingDaySection(i);
+      if (appointments[i].length > 0) {
+        showAppointments(i);
+      } else {
+        const appointmentDiv = document.getElementById("appointments");
+        appointmentDiv.style.display = "none";
+      }
     });
+    const cellValue = document.createElement("h3");
+    cellValue.innerText = i + 1;
+    dayDiv.appendChild(cellValue);
+    calendarDiv.appendChild(dayDiv);
+    appointments.push([]);
   }
 };
+
+createDays(dayInThisMonth());
+
+const handleFormSubmit = (e) => {
+  e.preventDefault();
+  const selectedDay = document.getElementById("new-meeting-day").innerText;
+  const meetingTime = document.getElementById("new-meeting-time").value;
+  const meetingName = document.getElementById("new-meeting-name").value;
+
+  const meetingString = `${meetingTime} - ${meetingName}`;
+  const indiceGiorno = parseInt(selectedDay) - 1;
+  appointments[indiceGiorno].push(meetingString);
+
+  const dot = document.createElement("span");
+  dot.classList.add("dot");
+
+  const selectedCell = document.querySelector(".selected");
+  if (!selectedCell.querySelector(".dot")) {
+    selectedCell.appendChild(dot);
+  }
+  showAppointments(indiceGiorno);
+};
+
+const meetingForm = document.querySelector("form");
+meetingForm.addEventListener("submit", handleFormSubmit);
