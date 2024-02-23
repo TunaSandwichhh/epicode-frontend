245958ff69022e9e6573099f355c997835edd312
TunaSandwich <davide.faggionato31@gmail.com>
2024-02-23 15:09:21 +0100

    Feat: implemented v2 project S50L5

--- UNIT-2/S5/S5-L5-project/assets/js/script.js
+++ UNIT-2/S5/S5-L5-project/assets/js/script.js
@@ -4,9 +4,19 @@ window.addEventListener("scroll", function () {
   let headerHeight = header.offsetHeight;
   let heroBottom = heroSection.offsetTop + heroSection.offsetHeight;
 
+  let aside = document.querySelector("aside");
+  let discoverySection = document.getElementById("discover");
+  let discoverySectionTop = discoverySection.offsetTop;
+
   if (window.scrollY > heroBottom - headerHeight) {
     header.classList.add("header-changed");
   } else {
     header.classList.remove("header-changed");
+  }
+
+  if (window.scrollY > discoverySectionTop) {
+    aside.classList.add("sticky-aside");
+  } else {
+    aside.classList.remove("sticky-aside");
   }
 });
